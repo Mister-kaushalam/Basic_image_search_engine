823dd95e11ceb58546da8a088d4808ca74eaa96f
Kaushal <kaushalbhavsar0007@gmail.com>
2020-06-03 12:16:34 +0530

    added searcher methods with distance matrix

---
+++ cbir/searcher.py
@@ -0,0 +1,43 @@
+#this will  handle comparing feature vectors, and thus the actual similarity of two images
+
+#import the necessary packages
+from . import dist
+import csv
+
+class Searcher:
+    def __init__(self, dbPath):
+        self.dbPath = dbPath
+
+    def search(self, queryFeatures, numResults=10):
+        #initalize the result directory
+        results={}
+
+        #open the database for reading
+        with open(self.dbPath) as f:
+            #initalize the csv reader
+            reader = csv.reader(f)
+
+            #loop over the rows in the index
+            for row in reader:
+                # parse out the image ID and features, then compute the chi-squared
+                # distance between the features in our database and the query features
+                features = [float(x) for x in row[1:]]
+                d= dist.chi2_distance(features, queryFeatures)
+
+                # now that we have the distance between the two feature vectors, we
+                # can update the results dictionary -- the key is the current image
+                # ID in the database and the value is the distance we just computed,
+                # representing how 'similar' the image in the database is to our query
+
+                results[row[0]] = d
+
+            #close the reader
+            f.close()
+
+        #sort our results, so that smaller distance are at the front of the lists
+        results = sorted([(v,k) for (k,v) in results.items()])
+
+        #return the results
+        return results
+
+        
