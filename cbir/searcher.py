4682a3dbf13001eb0cf971c795b46e7636d22484
Kaushal <kaushalbhavsar0007@gmail.com>
2020-06-03 13:37:18 +0530

    Minor issues fixed

--- cbir/searcher.py
+++ cbir/searcher.py
@@ -1,43 +1,40 @@
-#this will  handle comparing feature vectors, and thus the actual similarity of two images
-
-#import the necessary packages
+# import the necessary packages
 from . import dist
 import csv
 
 class Searcher:
-    def __init__(self, dbPath):
-        self.dbPath = dbPath
+	def __init__(self, dbPath):
+		# store the database path
+		self.dbPath = dbPath
 
-    def search(self, queryFeatures, numResults=10):
-        #initalize the result directory
-        results={}
+	def search(self, queryFeatures, numResults=10):
+		# initialize the results dictionary
+		results = {}
 
-        #open the database for reading
-        with open(self.dbPath) as f:
-            #initalize the csv reader
-            reader = csv.reader(f)
+		# open the database for reading
+		with open(self.dbPath) as f:
+			# initialize the CSV reader
+			reader = csv.reader(f)
 
-            #loop over the rows in the index
-            for row in reader:
-                # parse out the image ID and features, then compute the chi-squared
-                # distance between the features in our database and the query features
-                features = [float(x) for x in row[1:]]
-                d= dist.chi2_distance(features, queryFeatures)
+			# loop over the rows in the index
+			for row in reader:
+				# parse out the image ID and features, then compute the chi-squared
+				# distance between the features in our database and the query features
+				features = [float(x) for x in row[1:]]
+				d = dist.chi2_distance(features, queryFeatures)
 
-                # now that we have the distance between the two feature vectors, we
-                # can update the results dictionary -- the key is the current image
-                # ID in the database and the value is the distance we just computed,
-                # representing how 'similar' the image in the database is to our query
+				# now that we have the distance between the two feature vectors, we
+				# can update the results dictionary -- the key is the current image
+				# ID in the database and the value is the distance we just computed,
+				# representing how 'similar' the image in the database is to our query
+				results[row[0]] = d
 
-                results[row[0]] = d
+			# close the reader
+			f.close()
 
-            #close the reader
-            f.close()
+		# sort our results, so that the smaller distances (i.e. the more relevant images)
+		# are at the front of the list)
+		results = sorted([(v, k) for (k, v) in results.items()])
 
-        #sort our results, so that smaller distance are at the front of the lists
-        results = sorted([(v,k) for (k,v) in results.items()])
-
-        #return the results
-        return results
-
-        
+		# return the results
+		return results[:numResults]
--- search.py
+++ search.py
@@ -1,9 +1,12 @@
-#importing necessary packages
+# USAGE
+# python search.py --index index.csv --dataset ../ukbench --relevant ../ukbench/relevant.json \
+# 	--query ../ukbench/ukbench00980.jpg
 
+# import the necessary packages
 from __future__ import print_function
-from image_search_engine.cbir.resultmontage import ResultsMontage
-from image_search_engine.cbir.hsvdescriptor import HSVDescriptor
-from image_search_engine.cbir.searcher import Searcher
+from cbir.resultmontage import ResultsMontage
+from cbir.hsvdescriptor import HSVDescriptor
+from cbir.searcher import Searcher
 import argparse
 import imutils
 import json
@@ -17,33 +20,36 @@ ap.add_argument("-d", "--dataset", required=True, help="Path to the original dat
 ap.add_argument("-r", "--relevant", required=True, help = "Path to relevant dictionary")
 args = vars(ap.parse_args())
 
-#initialize the image descriptor and result montage
-desc = HSVDescriptor((4,6,3))
-montage = ResultsMontage((240,320), 5, 20)
+# initialize the image descriptor and results montage
+desc = HSVDescriptor((4, 6, 3))
+montage = ResultsMontage((240, 320), 5, 20)
 relevant = json.loads(open(args["relevant"]).read())
 
-#load the relevant querries dictionary and look up the relevant results for the querry image
-queryFileName = args["query"][args["query"].rfind("/") + 1 :]
-queryRelevant = relevant[queryFileName]
+# load the relevant queries dictionary and lookup the relevant results for the
+# query image
+queryFilename = args["query"][args["query"].rfind("/") + 1:]
+queryRelevant = relevant[queryFilename]
 
-#load the query imag, display it and describe it
+# load the query image, display it, describe it
 print("[INFO] describing query...")
 query = cv2.imread(args["query"])
 cv2.imshow("Query", imutils.resize(query, width=320))
 features = desc.describe(query)
 
-#performing the search
-print("[INFO] Searching...")
+# perform the search
+print("[INFO] linear_searching...")
 searcher = Searcher(args["index"])
 results = searcher.search(features, numResults=20)
 
-#loop over the results
+# loop over the results
 for (i, (score, resultID)) in enumerate(results):
-    #load the image and display it
-    print("[INFO] {result_num}. {result} - {score:.2f}".format(result_num=i + 1, result=resultID, score=score))
-    result = cv2.imread("{}/{}".format(args["dataset"], resultID))
-    montage.addResult(result, text="#{}".format(i + 1), highlight=resultID in queryRelevant)
- 
+    if i<= 20:
+        # load the result image and display it
+        print("[INFO] {result_num}. {result} - {score:.2f}".format(result_num=i + 1, result=resultID,
+            score=score))
+        result = cv2.imread("{}/{}".format(args["dataset"], resultID))
+        montage.addResult(result, text="#{}".format(i + 1), highlight=resultID in queryRelevant)
+
 # show the output image of results
 cv2.imshow("Results", imutils.resize(montage.montage, height=700))
 cv2.waitKey(0)
